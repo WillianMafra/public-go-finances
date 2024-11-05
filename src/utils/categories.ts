d047c98588b6e1772f8f9a52e2e7248737d1efba
Willian <williannunes965@gmail.com>
2024-11-05 16:34:30 -0300

    feat: adding some categories

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,8 +1,11 @@
 export const categories = [
     {key: 'purchases', name: 'Compras', icon: 'shopping-bag', color: '#5636D3'},
-    {key: 'food', name: 'Alimentação', icon: 'coffee', color: '#ff872c'},
+    {key: 'food', name: 'Mercado', icon: 'coffee', color: '#ff872c'},
     {key: 'salary', name: 'Salário', icon: 'dollar-sign', color: '#12a454'},
-    {key: 'car', name: 'Carro', icon: 'truck', color: '#e83f5b'},
+    {key: 'car', name: 'Transporte', icon: 'truck', color: '#e83f5b'},
     {key: 'leisure', name: 'Lazer', icon: 'heart', color: '#26195c'},
     {key: 'studies', name: 'Estudos', icon: 'book', color: '#9c001a'},
+    {key: 'home', name: 'Contas de Casa', icon: 'home', color: '#34a8eb'},
+    {key: 'debts', name: 'Dívidas', icon: 'meh', color: '#857f7d'},
+    {key: 'others', name: 'Outros', icon: 'help-circle', color: '#eb34ae'},
 ];
